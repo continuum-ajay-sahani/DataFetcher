16804620f619c37d51397e5071c29c19ab92d375
continuum-ajay-sahani <ajay.sahani@continuum.net>
2017-08-11 19:27:02 +0530

    Update urls.go

--- ifsc/urls.go
+++ ifsc/urls.go
@@ -1,19 +1,2 @@
 package ifsc
 
-const (
-	test = ""
-	//CBankNameURL provide all bank names
-	CBankNameURL = "http://jobwale.co.in/bankifsccode_services/ifsc.php"
-	//CStateNameURL provide all state name
-	CStateNameURL = "http://jobwale.co.in/bankifsccode_services/state.php?bank_name="
-	//CDistrictNameURL provide all district names
-	CDistrictNameURL = "http://jobwale.co.in/bankifsccode_services/district.php?bank_name="
-	//CBranchNameURL provide all bank branch information
-	CBranchNameURL = "http://jobwale.co.in/bankifsccode_services/branch.php?bank_name="
-	//CFinalResultURL provide final result
-	CFinalResultURL = "http://jobwale.co.in/bankifsccode_services/final_result.php?bank_name="
-	//CIFSCCodeURL fetch by using ifsc code
-	CIFSCCodeURL = "http://jobwale.co.in/bankifsccode_services/search_by_ifsccode.php?ifsc_code="
-	//CMICRCode  fetch by using micr code
-	CMICRCode = "http://jobwale.co.in/bankifsccode_services/search_by_micrcode.php?micr_code="
-)
